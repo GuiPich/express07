2d41dcb169a92b61ed9c1cbf3e1d0532401d48dd
GuiPich <110974962+GuiPich@users.noreply.github.com>
2022-12-12 12:00:49 +0100

    response express07

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 
+
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
@@ -24,9 +25,11 @@ app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const userHandlers = require("./userHandlers");
 
+const { hashPassword } = require("./auth");
+
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
